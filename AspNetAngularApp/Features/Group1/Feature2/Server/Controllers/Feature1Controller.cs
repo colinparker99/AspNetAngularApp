4235e0b4905fa79fd035ca0da2f80127b77aa43a
Colin <colinparker@live.com>
2018-01-07 14:47:22 -0500

    Initial commit for migrated project.

---
+++ AspNetAngularApp/Features/Group1/Feature2/Server/Controllers/Feature1Controller.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetAngularApp.Features.Group1.Feature1.Server.Controllers
+{
+    public class Feature1Controller : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("~/Features/Group1/Feature1/Client/index.cshtml");
+        }
+    }
+}
---
+++ AspNetAngularApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AspNetAngularApp.Global" Language="C#" %>
